6fe54005be32ac4c78bf1ff61a318128cbc5fbf1
Victorio B. Neto <victorio@go2doc.com.br>
2012-01-27 17:55:37 -0200

    ok

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,7 @@
 class Doctor
   include Mongoid::Document
   field :email, :type => String
+  validates_presence_of :email
   validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 end
 
--- app/models/home.rb
+++ app/models/home.rb
@@ -2,6 +2,6 @@ class Home
   include Mongoid::Document
   field :email, :type => String
 
-  #validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, :if => Proc.new{|home| home.emaildoc.blank? }
+  validates_presence_of :email
   validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 end
